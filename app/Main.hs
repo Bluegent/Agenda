780f391961279dddcd52d169f1a3af92a4e507da
Bluegent <bluegentleman@live.com>
2020-11-09 20:06:15 +0200

    Separated config file stuff

--- app/Main.hs
+++ app/Main.hs
@@ -8,29 +8,21 @@ import Data.Time
 import Misc.Config
 import Misc.Menu
 import Control.Monad
+import Control.Exception
+
+
 
 main :: IO ()
 main = do
-    val <- readfile emptyCP "docs/config.cfg"
-    let configParser = forceEither val
-    greetUser configParser
-{-|
-    d <- (eitherDecode <$> pathToString (getConfigPath  configParser "contacts_file")) :: IO (Either String [Contact])
-    case d of
-        Left err -> putStrLn err
-        Right ps -> printContactList ps
-        
-    r <- (eitherDecode <$> pathToString (getConfigPath  configParser "appointments_file")) :: IO (Either String [Appointment])
-    case r of
-        Left err -> putStrLn err
-        Right ps -> printAppointmentList ps
--}  
+    let defaultCfgPath = "docs/config.cfg" 
+    
+    cfgParser <- getCfgFromPath defaultCfgPath
+    greetUser cfgParser
 
-
-    contacts <- parseContacts $ getConfigPath  configParser "contacts_file"
+    contacts <- parseContacts $ getConfigPath  cfgParser "contacts_file"
     printContactList contacts
     
-    appts <- parseAppointments $ getConfigPath  configParser "appointments_file"
+    appts <- parseAppointments $ getConfigPath  cfgParser "appointments_file"
     printAppointmentList appts
     
     let loop = do 
--- src/Misc/Config.hs
+++ src/Misc/Config.hs
@@ -1,7 +1,36 @@
 module Misc.Config where
 
+import System.Directory
 import Data.Either.Utils
 import Data.ConfigFile
+
+defaultCfg :: ConfigParser
+defaultCfg = forceEither $ do
+    let cfg = emptyCP
+    cfg <- add_section cfg "PATHS"
+    cfg <- set cfg "PATHS" "contacts_file" "docs/contacts.json"
+    cfg <- set cfg "PATHS" "appointments_file" "docs/appointments.json"
+    cfg <- add_section cfg "MISC"
+    cfg <- set cfg "MISC" "callout" "User"
+    return cfg
+
+
+readCfg :: FilePath -> IO ConfigParser
+readCfg path = do
+    res <- readfile emptyCP path
+    let cp = forceEither res
+    return cp
+
+
+getCfgFromPath :: FilePath -> IO ConfigParser
+getCfgFromPath path = do
+    res <- doesFileExist path
+    if res 
+    then do
+        cfg <- readCfg path
+        return cfg
+    else return defaultCfg
+
 
 greetUser configParser = putStrLn $ "Hello, " ++ (forceEither $ get configParser "MISC" "callout") ++"!"
 
